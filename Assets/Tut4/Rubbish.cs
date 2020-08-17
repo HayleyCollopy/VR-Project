e65e4a1a4a0e8f463d779df26a19198661d5555a
HayleyCollopy <61652264+HayleyCollopy@users.noreply.github.com>
2020-08-17 13:44:01 +0800

    Update Rubbish.cs

--- Assets/Tut4/Rubbish.cs
+++ Assets/Tut4/Rubbish.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Rubbish : MonoBehaviour
 {
+    Material mat;
+    Color originalColour;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +16,9 @@ public class Rubbish : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void ChangeColour()
+    {
     }
 }
